a9223214bbdbc5a5a9856dcf8cc9b33c8d81f61c
dklota <143158882+dklota@users.noreply.github.com>
2025-10-17 12:15:26 -0700

    Refine motor control based on distance readings
    Updated distance thresholds and motor control logic for better navigation.

--- battleboats_code25.ino
+++ battleboats_code25.ino
@@ -3,20 +3,44 @@
 
 void setup() {
   Serial.begin(9600);
+  // initialize motor and ultrasonics
   motorInit();
   ultrasonicInit();
 }
 
 void loop() {
+  // delay
+  driveForward(255);
+  // start, drive forward 800 mm
+  // have ultrasonic sensor detect if the distance is 200 mm 
   float dist = readDistanceCM();
   Serial.print("Distance: ");
   Serial.print(dist);
   Serial.println(" cm");
 
-  if (dist > 15) {
-    driveForward(150);
+  if (dist > 20) {
+    driveForward(255);
   } else {
     driveStop();
+    // turn down the right motor a little bit
+    turnRight(150);
+    driveForward(150);
+  }
+
+  if (dist > 20) {
+    driveforward(100);
+  } else {
+    driveStop();
+    turnLeft(255);
+    driveForward(150);
+  }
+
+  if (dist > 20) {
+    driveforward(100);
+  } else {
+    driveStop();
+    turnRight(255);
+    driveForward(255);
   }
 
   delay(100);
